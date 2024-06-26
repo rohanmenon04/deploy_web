4188230b75fea530c41f695b0e877e61e6c67fa8
rohanmenon04 <156588188+rohanmenon04@users.noreply.github.com>
2024-06-27 00:47:27 +0200

    Update server.py

--- frontend/server/server.py
+++ frontend/server/server.py
@@ -11,6 +11,11 @@ app.secret_key = 'your_secret_key'  # Set a secret key for session management
 compress = Compress(app)
 
 # SSL Certificates
+context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+context.load_cert_chain(certfile='frontend/server/server.cert', keyfile='frontend/server/server.key')
+
+# Enable Brotli and Gzip compression
+app.config['COMPRESS_ALGORITHM'] = ['br', 'gzip']
 app.config['COMPRESS_BR_LEVEL'] = 11
 app.config['COMPRESS_GZIP_LEVEL'] = 6
 
@@ -26,17 +31,85 @@ Session(app)
 # Serve the landing page
 @app.route('/')
 def serve_landing_page():
+    return render_template('landing_page.html')
+
+# Serve static files and set Content-Encoding for Brotli files
+@app.route('/WebGLBuilds/<path:filename>')
+def serve_webgl_builds(filename):
+    response = send_from_directory('../../WebGLBuilds', filename)
+    if filename.endswith('.br'):
+        response.headers['Content-Encoding'] = 'br'
+    elif filename.endswith('.gz'):
+        response.headers['Content-Encoding'] = 'gzip'
+    return response
+
+# Serve favicon.ico from the specific directory
+@app.route('/favicon.ico')
+def serve_favicon():
+    return send_from_directory(os.path.join('../../WebGLBuilds', 'TemplateData'), 'favicon.ico')
+
+# Serve static files from 'static_files'
+@app.route('/static/<path:filename>')
+def serve_static_files(filename):
+    return send_from_directory('../static_files', filename)
+
+@app.route('/leaderboard')
+def serve_leaderboard():
+    backend_url_all_time = 'https://backend-service-fag8.onrender.com/api/leaderboard-all-time'
+    backend_url_24h = 'https://backend-service-fag8.onrender.com/api/leaderboard-24h'
+    
+    try:
+        leaderboard_response_all_time = requests.get(backend_url_all_time)
+        leaderboard_data_all_time = leaderboard_response_all_time.json()
+        time.sleep(1.05)
+        leaderboard_response_24h = requests.get(backend_url_24h)
+        leaderboard_data_24h = leaderboard_response_24h.json()
+        
+        return render_template('leaderboard.html', all_time_leaderboard=leaderboard_data_all_time, leaderboard_data_24h=leaderboard_data_24h)
+    except requests.exceptions.RequestException as e:
+        return str(e), 500
+
+@app.route('/pl-guide') 
+def serve_pl_guide():
+    return render_template('pl-guide.html')
 
 @app.route('/per-stats')
 def serve_per_stats():
     username = session.get('username')  # Retrieve the username from the session
     if not username:
         return "Username not found in session", 400
-
+    
     backend_url = f'https://backend-service-fag8.onrender.com/api/get-graph-data?username={username}'
-
+    
     try:
         response = requests.get(backend_url)
+        response.raise_for_status()  # Raise an exception for HTTP errors
+        data = response.json()
+        
+        # Check if data is empty and set up a default if necessary
+        if not data["labels"] and not data["values"]:
+            data = {
+                "labels": [],
+                "values": []
+            }
+        return render_template('per-stats.html', chart_data=data)
+    except requests.exceptions.RequestException as e:
+        app.logger.error(f"Error fetching graph data: {e}")
+        return str(e), 500
+    except json.JSONDecodeError as e:
+        app.logger.error(f"Error decoding JSON: {e}")
+        return "Error decoding JSON response from backend.", 500
+
+# Route to serve the game
+@app.route('/play')
+def serve_game():
+    return render_template('index.html')
+
+@app.route('/api/chat', methods=['POST'])
+def proxy_chat():
+    backend_url = 'https://backend-service-fag8.onrender.com/api/chat'
+    response = requests.post(backend_url, json=request.json)
+    return (response.content, response.status_code, response.headers.items())
 
 @app.route('/save_playerdata', methods=['POST'])
 def proxy_save_playerdata():
@@ -52,14 +125,16 @@ def proxy_save_playerdata():
         return jsonify(response.json()), response.status_code
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
+@app.route('/api/delete-score', methods=['POST'])
 def proxy_delete_score():
     data = request.json
     score_to_delete = data.get('score')
     username = session.get('username')  # Retrieve the username from the session
-
+    
     if not score_to_delete or not username:
         return jsonify({"error": "No score or username provided"}), 400
-
+    
     backend_url = 'https://backend-service-fag8.onrender.com/api/delete-score'  # Replace with your backend URL
 
     # Forward the request to the backend
